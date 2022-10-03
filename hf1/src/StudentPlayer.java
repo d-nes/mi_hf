93833db09e5c2008da9f0c77b8d32419a12111a0
Dénes Urbán <urband@edu.bme.hu>
2022-10-03 11:34:09 +0200

    figured out how to see state of board

--- hf1/src/StudentPlayer.java
+++ hf1/src/StudentPlayer.java
@@ -5,6 +5,16 @@ public class StudentPlayer extends Player{
 
     @Override
     public int step(Board board) {
+        int[][] boardLocal = board.getState();
+        /*
+        1-x, 2-o, 0-emtpy
+        
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 6; j++)
+            System.out.print(boardLocal[i][j]);
+            System.out.println();
+        }
+        */
         return 0;
     }
 }
