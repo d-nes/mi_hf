dd26971e413a5bb60a96cb8f1b51b57442042a15
Dénes Urbán <urband@edu.bme.hu>
2022-12-13 19:46:31 +0100

    pain

--- hf3/Solution.java
+++ hf3/Solution.java
@@ -1,19 +1,50 @@
 package hf3;
 
-public class Solution{
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
-	public static double getEntropy(int nCat1, int nCat2){
-		
-		return 0.0;
-	}
+public class Solution {
 
-	public static int[] getBestSeparation(int[][] features, boolean[] labels){
-		int[] answer = {0,0};
+    static int[][] trainingData = new int[1000][10];
 
-		return answer;
-	}
+    public static void readData() {
+        Scanner sc;
+        try {
+            sc = new Scanner(new File("hf3\\train.csv")); // TODO
+            int lines = 0;
+            while (sc.hasNext()) {
+                String[] values = sc.next().split(",");
+                for(int i = 0; i < values.length; i++){
+                    trainingData[lines++][i] = Integer.parseInt(values[i]);
+                }
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
 
-	public static void main(String[] args){
-		
-	}
+    public static double getEntropy(int nCat1, int nCat2) {
+        if(nCat1 == 0 || nCat2 == 0)
+            return 0.0;
+        else{
+            double p1 = (double) nCat1 / (nCat1 + nCat2);
+            double p2 = (double) nCat2 / (nCat1 + nCat2);
+            return -p1 * (Math.log(p1) / Math.log(2)) - p2 * (Math.log(p2) / Math.log(2));
+        }
+    }
+
+    public static int[] getBestSeparation(int[][] features, boolean[] labels) {
+        int[] answer = { 0, 0 };
+
+        return answer;
+    }  
+
+    public static void main(String[] args) {
+        readData();
+        System.out.println(trainingData);
+    }
+
 }
