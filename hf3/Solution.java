ea6acd9290341d701cf0abe6e6dd8d1ce4be2a30
Dénes Urbán <urband@edu.bme.hu>
2022-12-13 17:32:14 +0100

    files created for hf3

---
+++ hf3/Solution.java
@@ -0,0 +1,19 @@
+package hf3;
+
+public class Solution{
+
+	public static double getEntropy(int nCat1, int nCat2){
+		
+		return 0.0;
+	}
+
+	public static int[] getBestSeparation(int[][] features, boolean[] labels){
+		int[] answer = {0,0};
+
+		return answer;
+	}
+
+	public static void main(String[] args){
+		
+	}
+}
